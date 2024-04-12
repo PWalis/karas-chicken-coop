178a6675b98a28cfe6189ac57651fcd04b717f44
PWalis <pwilky1016@gmail.com>
2024-04-12 16:22:38 -0700

    fixed linting errors

--- app/shop/[id]/page.tsx
+++ app/shop/[id]/page.tsx
@@ -1,11 +1,13 @@
 // page to display one product and all the product pictures and details of the product
 
+import Image from "next/image";
+
 export default function ProductPage() {
   return (
     <main className="mt-28 flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="">
-          <img src="https://via.placeholder.com/600" alt="item" />
+          <Image src="https://via.placeholder.com/600" alt="item" />
         </div>
         <div className="">
           <h1>Item Name</h1>
