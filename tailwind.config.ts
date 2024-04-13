47fd319d89d4274dd7220c3a6656fe2e0a706d72
Daisy Pennartz <daisypenn99@gmail.com>
2024-04-12 19:00:07 -0700

    Changes to package.json

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,12 +27,12 @@ const config: Config = {
     },
   },
   plugins: [require("flowbite/plugin"), require('taos/plugin')],
-  
+
   safelist: [
     '!duration-[0ms]',
     '!delay-[0ms]',
     'html.js :where([class*="taos:"]:not(.taos-init))'
-  ]
+  ],
 
 };
 export default config;
