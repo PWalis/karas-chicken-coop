837c8df9df4ff05509e862181061456f4973bf3c
Daisy Pennartz <daisypenn99@gmail.com>
2024-04-18 13:03:39 -0700

    Links fixed

--- ui/landingPage/components/shopCarousel.jsx
+++ ui/landingPage/components/shopCarousel.jsx
@@ -1,15 +1,13 @@
 "use client";
 import { Carousel } from "@material-tailwind/react";
-import image1 from "../../assets/autumn_1.jpg";
-import image2 from "../../assets/mike_shirt1.jpg";
-import image3 from "../../assets/mike_shirt2.jpg";
-import image4 from "../../assets/autumn_shirt1.jpg";
 
-export function ShopCarousel() {
+
+
+export function ShopCarousel({ image1, image2, image3, image4 }) {
   return (
     <Carousel loop={true} autoplay={true} autoplayDelay={3000} transition={{ duration: .75 }} className="rounded-xl">
       <img
-        src={image4.src}
+        src={image1.src}
         alt="image 1"
         className="h-full w-full object-cover"
       />
@@ -24,7 +22,7 @@ export function ShopCarousel() {
         className="h-full w-full object-cover"
       />
             <img
-        src={image1.src}
+        src={image4.src}
         alt="image 3"
         className="h-full w-full object-cover"
       />
--- ui/landingPage/shopSection.tsx
+++ ui/landingPage/shopSection.tsx
@@ -1,23 +1,37 @@
 "use client";
 import { SectionWrapper } from "../assets/animation/section-wrapper";
-
+import mike_shirt1 from "../assets/mike_shirt1.jpg"
+import mike_shirt2 from "../assets/mike_shirt2.jpg"
+import autumn_shirt1 from "../assets/autumn_1.jpg"
+import autumn_shirt2 from "../assets/autumn_shirt1.jpg"
 import { ShopCarousel } from "./components/shopCarousel";
 export default function ShopSection() {
-    return (
-        <SectionWrapper>
-<section id="ShopSection" className="justify-center text-center px-4 py-8 mx-auto max-w-screen-lg">
-<h2 className="text-4xl md:text-5xl lg:text-7xl uppercase justify-center text-center mb-5 ">Shop Our Newest Styles</h2>
-        <div className="flex justify-center"> 
-        <div className="mx-auto">
-            <ShopCarousel></ShopCarousel>
-            </div>
-            
+  return (
+    <SectionWrapper>
+      <section
+        id="ShopSection"
+        className="justify-center text-center px-4 py-8 mx-auto max-w-screen-lg"
+      >
+        <h2 className="text-4xl md:text-5xl lg:text-7xl uppercase justify-center text-center mb-5 ">
+          Shop Our Newest Styles
+        </h2>
+        <div className="flex justify-center">
+          <div className="mx-auto">
+          <ShopCarousel image1={mike_shirt1} image2={autumn_shirt1} image3={autumn_shirt2} image4={mike_shirt2}></ShopCarousel>
+          </div>
         </div>
 
         <SectionWrapper>
-<button className="uppercase text-4xl bg-floc-gray text-white px-6 py-4 mt-4" role="button"><span className="text">Visit The Shop</span></button>
-</SectionWrapper>
-</section>
-</SectionWrapper>
-    );
-  }
+            <a href="/shop">
+          <button
+            className="uppercase text-4xl bg-floc-gray text-white px-6 py-4 mt-4"
+            role="button"
+          >
+            <span className="text">Visit The Shop</span>
+          </button>
+          </a>
+        </SectionWrapper>
+      </section>
+    </SectionWrapper>
+  );
+}
