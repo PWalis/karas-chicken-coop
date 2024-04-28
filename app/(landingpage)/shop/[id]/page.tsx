6591fbb2124bf96398028fe79654a5e03da87a7f
Daisy Pennartz <daisypenn99@gmail.com>
2024-04-28 16:50:15 -0700

    Update to UI Product Page

--- app/(landingpage)/shop/[id]/page.tsx
+++ app/(landingpage)/shop/[id]/page.tsx
@@ -6,6 +6,8 @@ import Footer from "@/ui/footer/footer";
 import autumn_shirt1 from "@/ui/assets/autumn_shirt1.jpg";
 import { ProductGallery } from "@/ui/shop/productGallery";
 import { FAQ } from "@/ui/shop/FAQ";
+import { Sizing } from "@/ui/shop/sizing";
+import { QuantityCounter } from "@/ui/shop/quantityCounter";
 
 export default function ProductPage() {
   return (
@@ -22,24 +24,24 @@ export default function ProductPage() {
             <div className="mb-5">
               <h2 className="text-6xl mb-2">Product Title</h2>
               <h3 className="max-w-[500px]">
-                {" "}
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Deleniti ullam, enim praesentium cum fugiat, soluta, debitis
                 voluptatibus possimus nobis corrupti
               </h3>
               <p className="text-floc-gray/60 pt-2">
-                {" "}
                 material is made 60% cotton 40% polyester
               </p>
             </div>
+            <div>
+            <Sizing></Sizing>
+            <QuantityCounter></QuantityCounter>
+            </div>
             <div className="flex mb-10 gap-3">
               <button className="px-4 py-2 w-fill bg-floc-yellow uppercase">
-                {" "}
-                buy now{" "}
+                buy now
               </button>
               <button className="border-[.20em] border-floc-gray px-4 py-2 uppercase">
-                {" "}
-                add to cart{" "}
+                add to cart
               </button>
             </div>
             <div className="max-w-[600px]">
