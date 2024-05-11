41340d6919438a4f2439a288172c0a905d1429dd
Daisy Pennartz <daisypenn99@gmail.com>
2024-05-10 17:59:50 -0700

    Fixed flock card sectionwrapper animation

--- ui/landingPage/components/ourFlockCard.tsx
+++ ui/landingPage/components/ourFlockCard.tsx
@@ -1,6 +1,4 @@
-import barnhouseraster from "../../assets/barnhouseraster.png";
 import Image from "next/image";
-import { SectionWrapper } from "@/ui/assets/animation/section-wrapper";
 
 interface Props {
   src: any;
@@ -24,14 +22,12 @@ export function FlockCard({ src, alt, name, description }: Props) {
       </a>
       <div className="drop-shadow-lg bg-white rounded-lg group-hover:drop-shadow-2xl ease-in-out transition-all duration-300">
       <div className="p-5 ease-in transition-opacity duration-300">
-        <SectionWrapper>
           <h4 className="mb-2 text-2xl font-bold tracking-tight text-floc-gray">
             {name},
           </h4>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
   {description}
         </p>
-        </SectionWrapper>
         </div>
       </div>
       </div>
