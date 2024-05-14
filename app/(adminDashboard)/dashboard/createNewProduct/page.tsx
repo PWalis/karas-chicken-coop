f80f4a0dbcf692fa7ebfbe05a50d4205682cd04c
PWalis <pwilky1016@gmail.com>
2024-05-13 20:01:17 -0700

    form errors

--- app/(adminDashboard)/dashboard/createNewProduct/page.tsx
+++ app/(adminDashboard)/dashboard/createNewProduct/page.tsx
@@ -5,13 +5,17 @@ import { CreateProductForm } from "@/ui/dashboard/createProductForm";
 export default async function CreateNewProductPage() {
   const categories = (await fetchCategories()) as any;
 
-  const isCategoriesArray = Array.isArray(categories)
-  const CategoryArray = isCategoriesArray ? categories : []
-  
+  const isCategoriesArray = Array.isArray(categories);
+  const CategoryArray = isCategoriesArray ? categories : [];
+
   return (
     <div className="w-full h-fit min-h-screen">
-      <div className="mt-28"> <h1 className="text-4xl my-auto text-center uppercase mb-4"> Create A New Product </h1></div>
-    <CreateProductForm categories={CategoryArray}/>
+      <div className="mt-28">
+        <h1 className="text-4xl my-auto text-center uppercase mb-4">
+          Create A New Product
+        </h1>
+      </div>
+      <CreateProductForm categories={CategoryArray} />
     </div>
   );
 }
